9f2ba6edfb6eb4a7fd7ea637a9d2b8a378cca9b7
Leyla Khalilli <leyla.kh07@gmail.com>
2025-10-27 23:50:55 +0400

    Add files via upload
    Level-1 Task 7

---
+++ level 1.7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+   cout<<"birinci ededi daxil edin:"<<endl;
+   cout<<"ikinci ededi daxil edin:"<<endl;
+   cin>>a>>b;
+   
+   cout<<"a="<<b<<endl;
+   cout<<"b="<<a<<endl;
+   
+
+    return 0;
+}
