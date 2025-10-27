3c0bfa30792a0e4bc8c0c5128c1456d05d5654d5
Leyla Khalilli <leyla.kh07@gmail.com>
2025-10-28 01:00:16 +0400

    Add files via upload
    Level-2 Task 2

---
+++ level 2.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"eded daxil edin:";
+    cin>>a;
+    
+    if(a>0){
+        cout<<"musbetdir"<<endl;
+    }
+    else if(a<0){
+        cout<<"menfidir"<<endl;
+    }
+    else{
+    cout<<"sifirdir"<<endl;
+    }
+    return 0;
+}
