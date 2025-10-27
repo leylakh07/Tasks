3cc322d7ffd2a777a4d2356dcc866fbeb3a59d55
Leyla Khalilli <leyla.kh07@gmail.com>
2025-10-27 22:14:36 +0400

    Add files via upload
    Level-1 Task 4

---
+++ level 1.4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+   int r;
+   cout<<"radiusu daxil edin:";
+   cin>>r;
+   
+   cout<<"dairenin sahesi:"<<r*r<<"pi"<<endl;
+   cout<<"cevresi:"<<2*r<<"pi"<<endl;
+    
+    
+return 0;
+}
