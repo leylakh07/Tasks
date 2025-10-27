4968b4f35572c189b2a22a1569da1963a9a495b8
Leyla Khalilli <leyla.kh07@gmail.com>
2025-10-27 23:55:54 +0400

    Add files via upload
    Level-1 Task 8

---
+++ level 1.8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"eded daxil edin:";
+    cin>>a;
+    
+    cout<<"kvadratin perimetri="<<a*4<<endl;
+    cout<<"kvadratin sahesi="<<a*a<<endl;
+}
