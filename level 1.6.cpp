cc6f538ce66584dcce421779192a7b61a93bd2df
Leyla Khalilli <leyla.kh07@gmail.com>
2025-10-27 22:15:24 +0400

    Add files via upload
    Level-1 Task 6

---
+++ level 1.6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+  
+  double a,b,c;
+  cout<<"ededleri daxil edin:";
+  cin>>a>>b>>c;
+  
+  cout<<"ededi ortasi="<<(a+b+c)/3<<endl;
+    
+    
+return 0;
+}
