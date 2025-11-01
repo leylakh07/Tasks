f52de56392475c6c55d224c6df2f9f2c4c05462f
Leyla Khalilli <leyla.kh07@gmail.com>
2025-11-01 21:42:58 +0400

    level 2 task 4

---
+++ level 2.4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+int a;
+cout<<"bir eded daxil edin:";
+cin>>a;
+
+if (a%2==0){
+    cout<<"eded cutdur"<<endl;
+}
+else{
+    cout<<"eded tekdir"<<endl;
+}
+return 0;
+}
