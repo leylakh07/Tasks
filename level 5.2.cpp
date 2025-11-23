e563376927f457a1bf26d6128425dfaaea194a11
Leyla Khalilli <leyla.kh07@gmail.com>
2025-11-23 20:40:12 +0400

    Add files via upload

---
+++ level 5.2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int cem(int a, int b){
+  return a+b;
+}
+int ferq(int a, int b){
+  return a-b;
+}
+int hasil(int a, int b){
+  return a*b;
+}
+int bolme(int a, int b){
+  return a/b;
+}
+int main() {
+  int a,b;
+  cout<<"iki eded daxil edin:";
+  cin>>a>>b;
+  
+  cout<<"cem:"<<cem(a,b)<<endl;
+  cout<<"ferq:"<<ferq(a,b)<<endl;
+  cout<<"hasil:"<<hasil(a,b)<<endl;
+  cout<<"bolme"<<bolme(a,b)<<endl;
+}
