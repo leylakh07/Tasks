10109714022565e54aa5d8d084473206735a6fea
Leyla Khalilli <leyla.kh07@gmail.com>
2025-11-24 00:35:40 +0400

    Update level 5.9(level 2 task 2).cpp

--- level 5.9(level 2 task 2).cpp
+++ level 5.9(level 2 task 2).cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 void yoxla(int a) {
     if (a > 0)
-        cout << "eded musbetdir." << endl;
+        cout<<"eded musbetdir."<<endl;
     else if (a < 0)
-        cout << "eded menfidir." << endl;
+        cout<<"eded menfidir."<<endl;
     else
-        cout << "eded sifirdir." << endl;
+        cout<<"eded sifirdir."<<endl;
 }
 
-int main() {
+int main(){
     int eded;
-    cout << "bir eded daxil edin: ";
-    cin >> eded;
+    cout<<"bir eded daxil edin: ";
+    cin>>eded;
 
     yoxla(eded);
     return 0;
 }
+
 
