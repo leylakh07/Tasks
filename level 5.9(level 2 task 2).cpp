2b082ca6c526e46c1d7fb798b0bb10aeb02ca783
Leyla Khalilli <leyla.kh07@gmail.com>
2025-11-24 00:22:48 +0400

    Add files via upload

---
+++ level 5.9(level 2 task 2).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void yoxla(int a) {
+    if (a > 0)
+        cout << "eded musbetdir." << endl;
+    else if (a < 0)
+        cout << "eded menfidir." << endl;
+    else
+        cout << "eded sifirdir." << endl;
+}
+
+int main() {
+    int eded;
+    cout << "bir eded daxil edin: ";
+    cin >> eded;
+
+    yoxla(eded);
+    return 0;
+}
+
